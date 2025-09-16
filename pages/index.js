bf48ee2c45484a1af8079c6a38b18fb0cade7c44
Tinniam V Ganesh <tvganesh.85@gmail.com>
2025-09-16 08:17:57 +0530

    16 Sep

--- pages/index.js
+++ pages/index.js
@@ -66,7 +66,7 @@ export default function Home() {
     'food & entertainment', 'mobile', 'pet care','salary','electricity','water','gas','personal care','car maintrenance','house maintrenance','clothes & accessories','health & fitness','other'
   ]);
   const [incomeCategories, setIncomeCategories] = useState([
-    'rent received', 'interest', 'annuity', 'salary', 'stock profit','stock dividend','corporate fd','bond interest','other'
+    'rent received', 'interest', 'annuity', 'salary', 'stock profit','stock dividend','corporate fd','bond interest','SWP','other'
   ]);
   const [showExpenseCategoryInput, setShowExpenseCategoryInput] = useState(false);
   const [showIncomeCategoryInput, setShowIncomeCategoryInput] = useState(false);
